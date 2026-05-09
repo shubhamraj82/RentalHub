4969ed02da00a39741e05efd07c6a6ee1dd323cb
shubhamraj82 <raj.shubh8216@gmail.com>
2026-05-09 21:05:08 +0530

    landing page done

---
+++ rentalhub/src/app/(nondashboard)/landing/FeaturesSection.tsx
@@ -0,0 +1,96 @@
+"use client"
+
+import React from 'react'
+import {motion} from "framer-motion"
+
+const containerVariants ={
+    hidden:{ opacity:0,y :50},
+    visible : {opacity:1,y:0, transition:{
+        duration:0.5,
+        staggerChildren:0.2,
+    },
+},
+}
+;
+
+const itemvariants ={
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+}
+
+const FeatureCard = ({
+    imageSrc,
+    title,
+    description,
+    linkText,
+    linkHref,
+  }: {
+    imageSrc: string;
+    title: string;
+    description: string;
+    linkText: string;
+    linkHref: string;
+  }) => (
+    <div className="text-center">
+      <div className="p-4 rounded-lg mb-4 flex items-center justify-center h-48">
+        <img src={imageSrc} className="w-full h-full object-contain" alt={title} />
+      </div>
+      <h3 className="text-xl font-semibold mb-2">{title}</h3>
+      <p className="mb-4">{description}</p>
+      <a
+        href={linkHref}
+        className="inline-block border border-gray-300 rounded px-4 py-2 hover:bg-gray-100"
+      >
+        {linkText}
+      </a>
+    </div>
+  );
+  
+
+const FeaturesSection = () => {
+  return (
+    <motion.div
+    initial="hidden"
+    whileInView="visible"
+    viewport={{once:true}}
+    variants={containerVariants}
+    className='py-24 px-6 sm:px-8 lg:px-12 xl:px-16 bg-white'
+    >
+        <div className='max-w-4xl xl:w-6xl mx-auto'>
+            <motion.h2
+            variants={itemvariants}
+            className='text-3xl font-bold text-center mb-12 w-full sm:w-2/3 mx-auto'
+            >
+                 Quickly find the home you want using our effective search filters!
+            </motion.h2>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-8 lg:gap-12 xl:gap-16'>
+                {[0,1,2].map((index)=>(
+                    <motion.div key={index} variants={itemvariants}>
+                       <FeatureCard
+                imageSrc={`/landing-search${3 - index}.png`}
+                title={
+                  [
+                    "Trustworthy and Verified Listings",
+                    "Browse Rental Listings with Ease",
+                    "Simplify Your Rental Search with Advanced",
+                  ][index]
+                }
+                description={
+                  [
+                    "Discover the best rental options with user reviews and ratings.",
+                    "Get access to user reviews and ratings for a better understanding of rental options.",
+                    "Find trustworthy and verified rental listings to ensure a hassle-free experience.",
+                  ][index]
+                }
+                linkText={["Explore", "Search", "Discover"][index]}
+                linkHref={["/explore", "/search", "/discover"][index]}
+              />
+                    </motion.div>
+                ))}
+            </div>
+        </div>
+    </motion.div>
+  )
+}
+
+export default FeaturesSection
